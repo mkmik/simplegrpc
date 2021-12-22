f28bf23078f83906bf5c7452453148e13fa90c90
Marko Mikulicic <mmikulicic@gmail.com>
2021-12-22 15:17:59 +0100

    channelz and binary log

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/bitnami-labs/flagenv"
 	pb "github.com/mkmik/simplegrpc/helloworld"
@@ -13,6 +14,9 @@ import (
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
+
+	"google.golang.org/grpc/binarylog"
+	channelz "google.golang.org/grpc/channelz/service"
 )
 
 var (
@@ -34,7 +38,20 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	return &pb.HelloReply{Message: fmt.Sprintf("Hello %s from %s", in.GetName(), *id)}, nil
 }
 
+func init() {
+	sink, err := binarylog.NewTempFileSink()
+	if err != nil {
+		panic(err)
+	}
+	binarylog.SetSink(sink)
+	grpc.EnableTracing = true
+}
+
 func mainE() error {
+
+	log.Printf("Serving http at %q", ":2211")
+	go http.ListenAndServe(":2211", nil)
+
 	const listenAddr = ":1122"
 	listener, err := net.Listen("tcp", listenAddr)
 	if err != nil {
@@ -46,6 +63,7 @@ func mainE() error {
 	reflection.Register(srv)
 	grpc_health_v1.RegisterHealthServer(srv, hserver)
 	pb.RegisterGreeterServer(srv, &server{})
+	channelz.RegisterChannelzServiceToServer(srv)
 
 	log.Printf("Serving id %q gRPC at %q", *id, listenAddr)
 	return srv.Serve(listener)
